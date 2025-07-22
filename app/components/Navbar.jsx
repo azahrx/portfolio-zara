110be3b3adc54a3ebd91e8933130283f7c7d48cd
zara <azahrx@gmail.com>
2025-07-22 15:51:51 +0700

    wip: dark mode

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
 
         <div className='flex items-center gap-4'>
           <button>
-            <Image src={assets.moon_icon} alt='' className='w-6' />
+            <Image src={assets.moon_icon} alt='' className='w-6 hidden' />
           </button>
           <a href='#contact' className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-ovo'>
             Contact <Image src={assets.arrow_icon} className='w-3' alt='Arrow'/>
